9accae3f9c351a03f68f28243b5c168ddf00931b
gmdisk <54530416+gmdisk@users.noreply.github.com>
2020-03-05 15:35:17 +0900

    전체 영화 목록 -> 주제별 영화 목록
    거의 다 된거 아닌지??

--- src/main/webapp/js/rec_list.js
+++ src/main/webapp/js/rec_list.js
@@ -2,28 +2,42 @@
  * 담당 : 천세문
  */
 
-var set;
-
 $(function () {
-	
-	set = 0;
-	
+
+var set = 0;
+
 	$("#r_button").click(function() {
+		
+		if(set >= 8){
+			alert('가장 뒤 입니다.')
+		}else{
+		
 		$(".movie").eq(set).hide();
 		$(".movie").eq(set+1).hide();
 		$(".movie").eq(set+2).hide();
 		$(".movie").eq(set+3).hide();
 		
-    });
-	
+		set = set + 4;
+		
+		}
+		
+	});
+
 	$("#l_button").click(function() {
-		$(".movie").eq(set).show();
-		$(".movie").eq(set+1).show();
-		$(".movie").eq(set+2).show();
-		$(".movie").eq(set+3).show();
+		
+		if(set <= 0){
+			alert('가장 앞 입니다.')
+		}else{
+		
+		$(".movie").eq(set-4).show();
+		$(".movie").eq(set-3).show();
+		$(".movie").eq(set-2).show();
+		$(".movie").eq(set-1).show();
+		
+		set = set - 4;
+		
+		}
 		
     });
-
+	
 });
-
-

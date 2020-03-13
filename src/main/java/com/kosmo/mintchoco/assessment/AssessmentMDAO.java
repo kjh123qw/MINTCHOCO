30a918b385182f7d5c6e1bf99f409486e3b84bc1
Noah-Choi <54530379+Noah-Choi@users.noreply.github.com>
2020-03-13 16:46:45 +0900

    - 평가글 관련 기능 추가
     - 평가글 등록, 삭제, 등록된 글에대한 좋아요 누름, 취소 등에 대한 처리 추가

--- src/main/java/com/kosmo/mintchoco/assessment/AssessmentMDAO.java
+++ src/main/java/com/kosmo/mintchoco/assessment/AssessmentMDAO.java
@@ -3,10 +3,12 @@ package com.kosmo.mintchoco.assessment;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.kosmo.mintchoco.common.JDBCUtil;
+import com.kosmo.view.assessment.AssessmentController;
 
 /*
  * 담당자 : 김정호, 천세문
@@ -191,5 +193,200 @@ public class AssessmentMDAO {
 				JDBCUtil.close(stmt, conn);
 			}
 		}
+		
+		
+	//게시물의 랭킹 1~3위 까지 조회(추천수가 기준 이상이면서)
+	public List<AssessmentMVO> getAssessRank(int movieNum, int count) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 getAssessRank()");
+		List<AssessmentMVO> assessList = new ArrayList<AssessmentMVO>();		
+		try 
+		{		
+			final String sql = "select * from (select * from ASSESSMENT_VIEW where MOVIE_NUMBER = ? and LIKES >= ? order by LIKES desc, ASSESS_REGDATE asc) where rownum <= ?";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, movieNum);
+			stmt.setInt(2, AssessmentController.ASS_LIKE_CNT_STANDARD);
+			stmt.setInt(3, count);
+			rs = stmt.executeQuery();
+		
+			while(rs.next()) 
+			{
+				AssessmentMVO assess = new AssessmentMVO();
+				assess.setAssessNum(rs.getString("ASSESS_ID"));
+				assess.setMovieNum(rs.getInt("MOVIE_NUMBER"));
+				assess.setAssessIndate(rs.getDate("ASSESS_REGDATE"));
+				assess.setLikes(rs.getInt("LIKES"));
+				assess.setHates(rs.getInt("HATES"));					
+				assessList.add(assess);
+			}
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("getAssessRank()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		
+		return assessList;
+	}
+	
+	//게시물의 해당 태그에 대한 갯수 조회
+	public int getTagCount(int movieNum, String tag) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 getTagCount()");
+		int iCount = 0;
+		
+		try 
+		{		
+			final String sql = "select count(*) from ASSESSMENT where MOVIE_NUMBER = ? and upper(ASSESS_CONTENT) like upper('%?%')";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, movieNum);
+			stmt.setString(2, tag);
+			rs = stmt.executeQuery();
+		
+			if(rs.next()) 
+				iCount = rs.getInt(1);
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("getTagCount()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		
+		return iCount;
+	}
+	
+	
+	public int insertTag(String tag)
+	{
+		return insertTag(tag, 0);
+	}
+	
+	//태크 테이블에 태그 추가
+	public int insertTag(String tag, int movieNum)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 insertTag()");
+		int iCount = 0;
+		
+		try 
+		{		
+			final String sql = "insert into TAG values(?, ?)";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, tag);
+			
+			if(0 == movieNum)
+				stmt.setNull(2, Types.INTEGER);
+			else
+				stmt.setInt(2, movieNum);
+			
+			iCount = stmt.executeUpdate();
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("insertTag()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		
+		return iCount;
+	}
+	
+	//태그맵핑 추가
+	public int insertTagMapping(String tag, int movieNum)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 inertTagMapping()");
+		int iCount = 0;
+		
+		try 
+		{		
+			final String sql = "insert into TAG_MAPPING values(?, ?)";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, tag);
+			stmt.setInt(2, movieNum);
+			
+			iCount = stmt.executeUpdate();
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("inertTagMapping()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+		
+		return iCount;
+	}
+	
+	//태크맵팅 삭제
+	public void deleteTagMapping(String tag, int movieNumber)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 deleteTagMapping()");
+		
+		try 
+		{		
+			final String sql = "delete from TAG_MAPPING where MOVIE_NUMBER = ? and upper(TAG_CONTENT) = upper('?')";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, movieNumber);
+			stmt.setString(2, tag);
+			
+			stmt.executeUpdate();
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("deleteTagMapping()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+	}
+	
+	//태그테이블의  해당 태그의 cnt + 1
+	public void updateTagUseCnt(String tag)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("===> JDBC로 updateTagUseCnt()");
+		
+		try 
+		{		
+			final String sql = "update TAG set cnt = cnt + 1 where upper(TAG_CONTENT) = upper(?)";
+			
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, tag);
+			stmt.executeUpdate();
+		} 
+		catch(Exception e) 
+		{
+			System.out.println("updateTagUseCnt()" + e);
+		}
+		finally 
+		{
+			JDBCUtil.close(rs, stmt, conn);
+		}
+	}
 	
 }
